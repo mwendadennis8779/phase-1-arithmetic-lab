98d9d830c4a6a7494422c26bc89ae3f59b22c70f
Dennis <mwendadennis8779@gmail.com>
2022-11-28 11:40:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1= 2;
+let num2 = 31;
+let multiply =num1*num2;
+
+let random= Math.floor(Math.random()+1);
+
+
+let num3=37;
+let num4=11;
+let mod =Math.ceil(num3 %num4);
+
+let max=Math.max(19,9,7,20,11);
